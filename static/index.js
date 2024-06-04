62c383b9574910415f8e6bdc3d24eec4f07e6e25
Yunjai, Lee <leeyunjai@circul.us>
2024-06-04 16:07:35 +0900

    Update index.js

--- static/index.js
+++ static/index.js
@@ -67,4 +67,264 @@ window.onload = function() {
     
     if ("serial" in navigator) alert("Your browser supports Web Serial API!");
     else document.getElementById("output").innerText = alert("Your browser does not support Web Serial API, the latest version of Google Chrome is recommended!");
+
+
+
+function findBlocks(data) {
+  if (data && typeof data === 'object') {
+    if ('block' in data) {
+      jdata = data['block'];
+      if(jdata['type'].includes('_dynamic')) {
+        updateSecondDropdown.call(workspace.getBlockById(jdata['id']), jdata['fields']['dir'], jdata['fields']['filename'])
+      }
+    }
+    for (const key in data) {
+      findBlocks(data[key]);
+    }
+  }
+}
+
+let update_block = () => {
+  $("#codecheck").html(saveBlock==JSON.stringify(Blockly.serialization.workspaces.save(workspace)) ? "" : "<i class='fa-solid fa-circle fa-fade'></i>");
+}
+
+const workspace = Blockly.inject("blocklyDiv", {
+  toolbox: lang=="en"?toolbox_en:toolbox_ko,
+  collapse: true,
+  comments: true,
+  disable: true,
+  maxBlocks: Infinity,
+  trashcan: true,
+  horizontalLayout: false,
+  toolboxPosition: "start",
+  css: true,
+  media: "../static/",
+  rtl: false,
+  scrollbars: true,
+  sounds: false,
+  oneBasedIndex: true,
+  grid: {
+    spacing: 20,
+    length: 3,
+    colour: '#ddd',
+    snap: true
+  },
+  zoom: {
+    controls: true,
+    wheel: false,
+    startScale: 0.7,
+    maxScale: 3,
+    minScale: 0.3,
+    scaleSpeed: 1.05,
+    pinch: true
+  },
+  move:{
+    scrollbars: {
+      horizontal: true,
+      vertical: true
+    },
+    drag: true,
+    wheel: true,
+  },
+  renderer:"zelos", // "zelos", "minimalist", "thrasos"
+  theme: Blockly.Theme.defineTheme('modest', {
+    'base': Blockly.Themes.Classic,
+    startHats: true,
+    fontStyle: {
+      'family': null,
+      'weight': null,
+      'size': 16,
+    },
+    blockStyles: {
+      logic_blocks: {
+        colourPrimary: '#B098CB',
+        colourSecondary: '#EDE7F6',
+        colorTertiary: '#B39DDB',
+      },
+      loop_blocks: {
+        colourPrimary: '#85B687',
+        colourSecondary: '#E8F5E9',
+        colorTertiary: '#66BB6A',
+      },
+      math_blocks: {
+        colourPrimary: '#2196F3',
+        colourSecondary: '#1E88E5',
+        colorTertiary: '#0D47A1',
+      },
+      text_blocks: {
+        colourPrimary: '#FFAA08',
+        colourSecondary: '#555555',
+        colorTertiary: '#FF8F00',
+      },
+      list_blocks: {
+        colourPrimary: '#4DB6AC',
+        colourSecondary: '#B2DFDB',
+        colorTertiary: '#009688',
+      },
+      colour_blocks: {
+        colourPrimary: '#DFADB2',
+        colourSecondary: '#FFEBEE',
+        colorTertiary: '#EF9A9A',
+      },
+      variable_blocks: {
+        colourPrimary: '#EF9A9A',
+        colourSecondary: '#EF9A9A',
+        //colourSecondary: '#FFEBEE',
+        colorTertiary: '#EF5350',
+      },
+      // variable_dynamic_blocks: {
+      //   colourPrimary: '#EF9A9A',
+      //   colourSecondary: '#FFEBEE',
+      //   colorTertiary: '#EF5350',
+      // },
+      procedure_blocks: {
+        colourPrimary: '#C7BCB8',
+        colourSecondary: '#EFEBE9',
+        colorTertiary: '#BCAAA4',
+      },
+    },
+    categoryStyles: {
+      // logic_category: {
+      //   colour: '#D1C4E9'
+      // },
+      // loop_category: {
+      //   colour: '#A5D6A7'
+      // },
+      // math_category: {
+      //   colour: '#2196F3'
+      // },
+      // text_category: {
+      //   colour: '#FFCA28'
+      // },
+      // list_category: {
+      //   colour: '#4DB6AC'
+      // },
+      // colour_category: {
+      //   colour: '#FFCDD2'
+      // },
+      // variable_category: {
+      //   colour: '#EF9A9A'
+      // },
+      // // variable_dynamic_category: {
+      // //   colour: '#EF9A9A'
+      // // },
+      // procedure_category: {
+      //   colour: '#D7CCC8'
+      // }
+    },
+    componentStyles: {
+      'flyoutOpacity': 0.5,
+      'insertionMarkerOpacity': 0.5,
+      'scrollbarOpacity': 0.5,
+      'selectedGlowColour': '#000000',
+      'selectedGlowSize': 0.5,
+      'replacementGlowColour': '#000000',
+    }
+  }),
+});
+workspace.addChangeListener ((event)=>{
+  update_block();
+  if (event.type == Blockly.Events.CREATE) {
+    if($("#codepath").html() == '') setTimeout(()=>{alert(translations["confirm_block_file"][lang])},500);
+  }
+
+  if (event.type == Blockly.Events.BLOCK_CHANGE) {
+    if (event.element == 'field' && event.name == 'dir') {
+      folderValue = workspace.getBlockById(event.blockId).getFieldValue('dir');
+      updateSecondDropdown.call(workspace.getBlockById(event.blockId), folderValue);
+    }
+  }
+});
+
+$(document).keydown((evt)=> {
+  if((evt.which == '115' || evt.which == '83') && (evt.ctrlKey || evt.metaKey)) {
+      evt.preventDefault();
+      let filepath = $("#codepath").html();
+
+      if (filepath == "") {
+        alert(translations['nofile'][lang]);
+        return;
+      }
+      let codetype = "";
+      codeTypeBtns.forEach((el) => {
+        if (el.classList.value.includes("checked")) codetype = el.name;
+      });
+      if (codetype == "block") {
+        // if (filepath.substring(filepath.lastIndexOf(".") + 1, filepath.length) != "json") {
+        //   alert("json 파일만 저장 가능합니다.");
+        //   return;
+        // }
+        saveBlock = JSON.stringify(Blockly.serialization.workspaces.save(workspace))
+        socket.emit("save", {
+          codepath: "/home/pi/blockly.py",
+          codetext: Blockly.Python.workspaceToCode(workspace) });
+        socket.emit("save", {
+          codepath: $("#codepath").html(),
+          codetext: saveBlock
+        });
+        result.value = Blockly.Python.workspaceToCode(workspace);
+        update_block();
+      }
+      else {
+        codeTypeBtns.forEach((el) => {
+          if (el.classList.value.includes("checked")) codetype = el.name;
+        });
+        saveCode = codeEditor.getValue();
+        CodeMirror.signal(codeEditor, "change");
+        socket.emit("save", { codepath: $("#codepath").html(), codetext: saveCode });
+      }
+      return false;
+  }
+  return true;
+});
+
+const setLanguage = (langCode) => {
+  const elements = document.querySelectorAll('[data-key]');
+  elements.forEach(element => {
+      const key = element.getAttribute('data-key');
+      if (translations[key] && translations[key][langCode]) {
+          element.textContent = translations[key][langCode];
+      }
+  });
+
+  const langFileVersion = '240110v1';
+  const langFile = `../static/${langCode}.js?ver=${langFileVersion}`;
+  const prevKoScript = document.querySelector(`script[src*="../static/ko.js?ver=${langFileVersion}"]`);
+  if (prevKoScript) {
+    prevKoScript.remove();
+  }
+  const prevEnScript = document.querySelector(`script[src*="../static/en.js?ver=${langFileVersion}"]`);
+  if (prevEnScript) {
+    prevEnScript.remove();
+  }
+  const script = document.createElement('script');
+  script.setAttribute('src', langFile);
+  document.head.appendChild(script);
+  workspace.updateToolbox(langCode=="en"?toolbox_en:toolbox_ko);
+
+  if (langCode == "en") {
+    document.getElementById('add_directory').style.width = '105px';
+    document.getElementById('add_file').style.width = '100px';
+  }
+  else {
+    document.getElementById('add_directory').style.width = '80px';
+    document.getElementById('add_file').style.width = '85px';
+  }
+}
+
+const language = document.getElementById("language");
+language.value = lang;
+setLanguage(lang);
+localStorage.setItem("language", lang);
+
+language.addEventListener("change", () => {
+  lang = language.value;
+  setLanguage(lang);
+  localStorage.setItem("language", lang);
+});
+
+
+
+
+
 }
