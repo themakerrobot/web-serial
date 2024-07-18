edfe0318f36fcbb1212af51e682bb8140c78a533
leeyunjai82 <leeyunjai@circul.us>
2024-07-19 08:50:35 +0900

    update

---
+++ uart_ctrl.py
@@ -0,0 +1,90 @@
+import asyncio
+import time
+import serial
+
+ser = serial.Serial('/dev/ttyS0', 115200, timeout=1)
+filename = '/home/pi/tmp.py'
+current_process = None
+record = ''
+
+
+async def run_code(filename):
+  global current_process
+  try:
+    print('run_code')
+    start_time = time.time()
+    process = await asyncio.create_subprocess_exec(
+      'python3', '-u', filename,
+      stdout=asyncio.subprocess.PIPE,
+      stderr=asyncio.subprocess.STDOUT,
+      env={'PYTHONUNBUFFERED': '1'}
+    )
+    current_process = process
+
+    while True:
+      line = await process.stdout.readline()
+      if not line:
+        break
+      line = line.decode('utf-8').strip()
+      ser.write(line.encode('utf-8') + b'\n')
+      print(line)
+      await asyncio.sleep(0.01)
+
+    await process.wait()
+    end_time = time.time()
+    execution_time = end_time - start_time
+    
+    time_msg = f"실행시간: {execution_time:.2f} 초"
+    ser.write(time_msg.encode('utf-8') + b'\n')
+    ser.flush()
+    print(time_msg)
+    current_process = None
+  except Exception as e:
+    error_message = f"Error: {str(e)}"
+    print(error_message)
+    ser.write(error_message.encode('utf-8'))
+
+async def read_serial():
+  global current_process
+
+  while True:
+    if ser.in_waiting:
+      lines = ser.readlines()
+      print(lines)
+      
+      if len(lines) > 0:
+        if any('###END###' in line.decode('utf-8') for line in lines):
+          if current_process is not None:
+            print('정지버튼-start')
+            current_process.terminate()
+            try:
+              await asyncio.wait_for(current_process.wait(), timeout=1.0)
+            except asyncio.TimeoutError:
+              current_process.kill()
+            current_process = None
+            print('정지버튼-end')
+          continue
+        with open(filename, 'w') as file:
+          file.write('# -*- coding: utf-8 -*-\n')
+          for line in lines:
+            decoded_line = line.decode('utf-8')
+            file.write(decoded_line)
+        print('File save ok')
+   
+        if current_process != None:
+          current_process.terminate()
+          try:
+            await asyncio.wait_for(current_process.wait(), timeout=1.0)
+          except asyncio.TimeoutError:
+            current_process.kill()
+          current_process = None
+          print('prior processor exit')
+        asyncio.create_task(run_code(filename))
+    
+    await asyncio.sleep(0.1)
+
+async def main():
+  await read_serial()
+
+if __name__ == "__main__":
+  asyncio.run(main())
