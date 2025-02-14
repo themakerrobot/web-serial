832234aa4c5508f5e13ec74147004fdc4e71f693
Yunjai, Lee <leeyunjai@circul.us>
2025-02-14 10:06:59 +0900

    Update index.js

--- static/index.js
+++ static/index.js
@@ -26,7 +26,7 @@ window.onload = function() {
     const pythonEditor = CodeMirror(pythonEditorDiv, {
         mode: "python",
         theme: "monokai",
-        lineNumbers: true,
+        lineNumbers: "true",
         extraKeys: {
           "Ctrl-/": "toggleComment"
         },        
